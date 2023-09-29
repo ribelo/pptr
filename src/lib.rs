8a6936ce9a5817315fb243a07473c2b097922ccf
Rafał Krzyważnia <r.krzywaznia@gmail.com>
2023-09-29 23:25:52 +0200

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,44 @@
 use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
+    any::{Any, TypeId},
+    fmt::Debug,
+    future::Future,
+    sync::Arc,
 };
 
-use std::any::{Any, TypeId};
-
 use async_trait::async_trait;
-use tokio::task::JoinHandle;
+use dashmap::DashMap;
+use thiserror::Error;
+
+pub trait Message: Debug + Send + Sync + 'static {}
 
 #[async_trait]
-pub trait Event: Send + Sync {
-    async fn handle(&self, ox_frame: &OxFrame) -> Option<Vec<Box<dyn Event>>>;
+pub trait Actor: Send + 'static {
+    async fn handle(&mut self, ox: OxFrame, msg: Box<dyn Message>);
+}
+
+struct ActorHandle {
+    rx: tokio::sync::mpsc::UnboundedReceiver<Box<dyn Message>>,
+}
+
+async fn run_actor<S>(mut handle: ActorHandle, mut actor: impl Actor, ox: OxFrame)
+where
+    S: Send + 'static,
+{
+    println!("run_actor");
+    while let Some(msg) = handle.rx.recv().await {
+        println!("got msg: {:?}", msg);
+        actor.handle(ox.clone(), msg).await;
+    }
 }
 
 #[derive(Clone)]
 pub struct OxFrame {
-    inner: Arc<Mutex<OxFrameInner>>,
+    inner: Arc<OxFrameInner>,
 }
 
-pub struct OxFrameInner {
-    event_bus: EventBus,
-    context: HashMap<TypeId, Box<dyn Any + Send + 'static>>,
-}
-
-pub struct EventBus {
-    tx: tokio::sync::mpsc::UnboundedSender<Box<dyn Event>>,
-    rx: Option<tokio::sync::mpsc::UnboundedReceiver<Box<dyn Event>>>,
+struct OxFrameInner {
+    txs: DashMap<TypeId, tokio::sync::mpsc::UnboundedSender<Box<dyn Message>>>,
+    context: DashMap<TypeId, Box<dyn Any + Send + Sync + 'static>>,
 }
 
 impl Default for OxFrame {
@@ -34,88 +47,41 @@ impl Default for OxFrame {
     }
 }
 
+#[derive(Error, Debug)]
+pub enum OxFrameError {
+    #[error("actor already spawned")]
+    ActorAlreadySpawned,
+}
+
 impl OxFrame {
     pub fn new() -> Self {
-        let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
-        let event_bus = EventBus { tx, rx: Some(rx) };
-        OxFrame {
-            inner: Arc::new(Mutex::new(OxFrameInner {
-                event_bus,
-                context: HashMap::new(),
-            })),
+        Self {
+            inner: Arc::new(OxFrameInner {
+                txs: DashMap::new(),
+                context: DashMap::new(),
+            }),
         }
     }
 
-    pub fn dispatch(&self, event: impl Event + 'static) {
-        self.dispatch_boxed(Box::new(event));
-    }
-
-    fn dispatch_boxed(&self, event: Box<dyn Event>) {
-        self.inner.lock().unwrap().event_bus.tx.send(event).unwrap();
-    }
-
-    pub fn provide_context<T: Any + Send + 'static>(&self, value: T) {
-        self.inner
-            .lock()
-            .unwrap()
-            .context
-            .insert(TypeId::of::<T>(), Box::new(value));
-    }
-
-    pub fn use_context<T: Any + Send + Clone + 'static>(&self) -> Option<T> {
-        let context = &self.inner.lock().unwrap().context;
-        context
-            .get(&TypeId::of::<T>())
-            .and_then(|v| v.downcast_ref::<T>())
-            .cloned()
-    }
-
-    pub fn with_context<T, F, R>(&self, f: F) -> Option<R>
-    where
-        T: Any + Send + 'static,
-        F: FnOnce(&T) -> R,
-    {
-        let context = &self.inner.lock().unwrap().context;
-        context
-            .get(&TypeId::of::<T>())
-            .and_then(|v| v.downcast_ref::<T>().map(f))
-    }
-
-    pub fn with_context_mut<T, F, R>(&self, f: F) -> Option<R>
-    where
-        T: Any + Send + 'static,
-        F: FnOnce(&mut T) -> R,
-    {
-        let context = &mut self.inner.lock().unwrap().context;
-        context
-            .get_mut(&TypeId::of::<T>())
-            .and_then(|v| v.downcast_mut::<T>().map(f))
-    }
-}
-
-pub async fn run(ox_frame: &OxFrame) -> JoinHandle<()> {
-    let ox_clone = ox_frame.clone();
-    tokio::spawn(async move {
-        let mut rx = ox_clone.inner.lock().unwrap().event_bus.rx.take().unwrap();
-        while let Some(event) = rx.recv().await {
-            if let Some(events) = event.handle(&ox_clone).await {
-                for event in events {
-                    ox_clone.dispatch_boxed(event);
-                }
-            }
+    pub fn spawn<M: Message>(&self, actor: impl Actor) -> Result<(), OxFrameError> {
+        if let Some(_tx) = self.inner.txs.get(&TypeId::of::<M>()) {
+            Err(OxFrameError::ActorAlreadySpawned)
+        } else {
+            let id = TypeId::of::<M>();
+            println!("spawn actor: {:?}", id);
+            let (tx, rx) = tokio::sync::mpsc::unbounded_channel::<Box<dyn Message>>();
+            let handler = ActorHandle { rx };
+            tokio::spawn(run_actor::<M>(handler, actor, self.clone()));
+            println!("spawned actor: {:?}", id);
+            self.inner.txs.insert(id, tx);
+            Ok(())
         }
-    })
-}
+    }
 
-pub struct TestEvent {}
-
-#[async_trait]
-impl Event for TestEvent {
-    async fn handle(&self, ox_frame: &OxFrame) -> Option<Vec<Box<dyn Event>>> {
-        println!("handle test event");
-        let foo = ox_frame.use_context::<String>();
-        dbg!(foo);
-        None
+    pub fn send<M: Message>(&self, msg: M) {
+        if let Some(tx) = self.inner.txs.get(&TypeId::of::<M>()) {
+            tx.send(Box::new(msg)).unwrap();
+        }
     }
 }
 
@@ -126,11 +92,29 @@ mod tests {
 
     #[tokio::test]
     async fn it_works() {
+        #[derive(Debug)]
+        pub struct TestMessage {};
+
+        impl Message for TestMessage {};
+
+        #[derive(Debug)]
+        pub struct TestActor {
+            i: i32,
+        }
+
+        #[async_trait]
+        impl Actor for TestActor {
+            async fn handle(&mut self, ox: OxFrame, msg: Box<dyn Message>) {
+                self.i += 1;
+                println!("TestActor::handle {}", self.i);
+                ox.send(TestMessage {});
+            }
+        }
+
         let ox_frame = OxFrame::new();
-        run(&ox_frame).await;
-        ox_frame.provide_context::<String>("bar".to_string());
-        let test_event = TestEvent {};
-        ox_frame.dispatch(test_event);
-        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+        ox_frame.spawn::<TestMessage>(TestActor { i: 0 }).unwrap();
+        ox_frame.send(TestMessage {});
+        tokio::time::sleep(std::time::Duration::from_secs(3)).await;
     }
 }
