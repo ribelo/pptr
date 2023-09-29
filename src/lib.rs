46f6f60cbb81cafc9915f5388b5bfd5b86ecab30
Rafał Krzyważnia <r.krzywaznia@gmail.com>
2023-09-29 23:47:45 +0200

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,46 @@
 use std::{
     any::{Any, TypeId},
+    collections::HashMap,
     fmt::Debug,
     future::Future,
-    sync::Arc,
+    sync::{Arc, RwLock},
 };
 
 use async_trait::async_trait;
 use dashmap::DashMap;
 use thiserror::Error;
 
-pub trait Message: Debug + Send + Sync + 'static {}
+pub trait AsAny {
+    fn as_any(&self) -> &dyn Any;
+}
+
+impl<T: Any> AsAny for T {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+pub trait Message: AsAny + Debug + Send + Sync + 'static {}
 
 #[async_trait]
-pub trait Actor: Send + 'static {
-    async fn handle(&mut self, ox: OxFrame, msg: Box<dyn Message>);
+pub trait Actor<M>: Send + 'static {
+    async fn handle(&mut self, ox: OxFrame, msg: &M);
 }
 
 struct ActorHandle {
     rx: tokio::sync::mpsc::UnboundedReceiver<Box<dyn Message>>,
 }
 
-async fn run_actor<S>(mut handle: ActorHandle, mut actor: impl Actor, ox: OxFrame)
-where
-    S: Send + 'static,
-{
-    println!("run_actor");
+async fn run_actor<M: Message>(mut handle: ActorHandle, mut actor: impl Actor<M>, ox: OxFrame) {
     while let Some(msg) = handle.rx.recv().await {
-        println!("got msg: {:?}", msg);
-        actor.handle(ox.clone(), msg).await;
+        match (*msg).as_any().downcast_ref::<M>() {
+            Some(msg) => {
+                actor.handle(ox.clone(), msg).await;
+            }
+            None => {
+                println!("unknown message type, not {:#?}, ", msg);
+            }
+        }
     }
 }
 
@@ -37,8 +50,8 @@ pub struct OxFrame {
 }
 
 struct OxFrameInner {
-    txs: DashMap<TypeId, tokio::sync::mpsc::UnboundedSender<Box<dyn Message>>>,
-    context: DashMap<TypeId, Box<dyn Any + Send + Sync + 'static>>,
+    txs: RwLock<HashMap<TypeId, tokio::sync::mpsc::UnboundedSender<Box<dyn Message>>>>,
+    context: RwLock<HashMap<TypeId, Box<dyn Any + Send + Sync + 'static>>>,
 }
 
 impl Default for OxFrame {
@@ -57,29 +70,30 @@ impl OxFrame {
     pub fn new() -> Self {
         Self {
             inner: Arc::new(OxFrameInner {
-                txs: DashMap::new(),
-                context: DashMap::new(),
+                txs: RwLock::new(HashMap::new()),
+                context: RwLock::new(HashMap::new()),
             }),
         }
     }
 
-    pub fn spawn<M: Message>(&self, actor: impl Actor) -> Result<(), OxFrameError> {
-        if let Some(_tx) = self.inner.txs.get(&TypeId::of::<M>()) {
-            Err(OxFrameError::ActorAlreadySpawned)
-        } else {
-            let id = TypeId::of::<M>();
-            println!("spawn actor: {:?}", id);
-            let (tx, rx) = tokio::sync::mpsc::unbounded_channel::<Box<dyn Message>>();
-            let handler = ActorHandle { rx };
-            tokio::spawn(run_actor::<M>(handler, actor, self.clone()));
-            println!("spawned actor: {:?}", id);
-            self.inner.txs.insert(id, tx);
-            Ok(())
+    pub fn spawn<M: Message>(&self, actor: impl Actor<M>) -> Result<(), OxFrameError> {
+        let id = TypeId::of::<M>();
+        let mut write_guard = self.inner.txs.write().unwrap();
+
+        if write_guard.contains_key(&id) {
+            return Err(OxFrameError::ActorAlreadySpawned);
         }
+
+        let (tx, rx) = tokio::sync::mpsc::unbounded_channel::<Box<dyn Message>>();
+        let handler = ActorHandle { rx };
+        tokio::spawn(run_actor::<M>(handler, actor, self.clone()));
+        write_guard.insert(id, tx);
+
+        Ok(())
     }
 
     pub fn send<M: Message>(&self, msg: M) {
-        if let Some(tx) = self.inner.txs.get(&TypeId::of::<M>()) {
+        if let Some(tx) = self.inner.txs.read().unwrap().get(&TypeId::of::<M>()) {
             tx.send(Box::new(msg)).unwrap();
         }
     }
@@ -93,7 +107,9 @@ mod tests {
     #[tokio::test]
     async fn it_works() {
         #[derive(Debug)]
-        pub struct TestMessage {};
+        pub struct TestMessage {
+            i: i32,
+        };
 
         impl Message for TestMessage {};
 
@@ -103,18 +119,18 @@ mod tests {
         }
 
         #[async_trait]
-        impl Actor for TestActor {
-            async fn handle(&mut self, ox: OxFrame, msg: Box<dyn Message>) {
+        impl Actor<TestMessage> for TestActor {
+            async fn handle(&mut self, ox: OxFrame, msg: &TestMessage) {
                 self.i += 1;
-                println!("TestActor::handle {}", self.i);
-                ox.send(TestMessage {});
+                println!("TestActor::handle {}, msg i {}", self.i, msg.i);
+                ox.send(TestMessage { i: self.i });
             }
         }
 
         let ox_frame = OxFrame::new();
 
         ox_frame.spawn::<TestMessage>(TestActor { i: 0 }).unwrap();
-        ox_frame.send(TestMessage {});
+        ox_frame.send(TestMessage { i: 0 });
         tokio::time::sleep(std::time::Duration::from_secs(3)).await;
     }
 }
